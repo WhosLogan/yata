4b6f85ee145a15d373a4a8c2117981055a67691c
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2023-12-19 20:14:22 -0500

    Add database migrator

---
+++ src/lib/server/drizzle/migrate.js
@@ -0,0 +1,25 @@
+import { migrate } from "drizzle-orm/libsql/migrator";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+
+export const client = createClient({
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle(client);
+
+async function main() {
+    try {
+        await migrate(db, {
+            migrationsFolder: "./migrations",
+        });
+        console.log("Tables migrated!");
+        process.exit(0);
+    } catch (error) {
+        console.error("Error performing migration: ", error);
+        process.exit(1);
+    }
+}
+
+main();
