19291ff3e35a612c8f0f128b6b4a2fccbcaa672d
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2023-12-19 21:51:41 -0500

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {page} from "$app/stores";
+</script>
+
+<div class="flex h-screen justify-center items-center space-x-2">
+    <h2 class="text-3xl">{$page.status} | {$page.error?.message}</h2>
+</div>
