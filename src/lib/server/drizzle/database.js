714bd44644da69a001ba41cf58a6911f0c67eb32
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2023-12-19 20:13:53 -0500

    Add database client

---
+++ src/lib/server/drizzle/database.js
@@ -0,0 +1,9 @@
+import {drizzle} from "drizzle-orm/libsql";
+import {DATABASE_URL, DATABASE_AUTH_TOKEN} from "$env/static/private"
+import {createClient} from "@libsql/client";
+
+const db = new drizzle(createClient({
+    url: DATABASE_URL,
+    authToken: DATABASE_AUTH_TOKEN
+}));
+export default db;
