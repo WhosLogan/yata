912c668b18401facfffd43d50a87348e327ccaff
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2023-12-30 16:42:08 -0500

    Add the schema to the database client

--- src/lib/server/drizzle/database.js
+++ src/lib/server/drizzle/database.js
@@ -1,10 +1,11 @@
 import {drizzle} from "drizzle-orm/libsql";
 import {DATABASE_URL, DATABASE_AUTH_TOKEN} from "$env/static/private"
 import {createClient} from "@libsql/client";
+import * as schema from "./schema.js";
 
 const db = new drizzle(createClient({
     url: DATABASE_URL,
     authToken: DATABASE_AUTH_TOKEN
-}));
+}), { schema });
 
 export default db;
