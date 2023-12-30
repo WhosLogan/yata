dbb8cd39ec53af6b652586816c6d56d665998c57
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2023-12-30 16:41:25 -0500

    Finish register page

--- src/routes/register/+page.server.js
+++ src/routes/register/+page.server.js
@@ -1,5 +1,12 @@
+import db from "$lib/server/drizzle/database.js";
+import {users} from "$lib/server/drizzle/schema.js";
+import {fail, redirect} from "@sveltejs/kit";
+import * as argon2 from "argon2"
+import * as jose from "jose"
+import {AUTH_SECRET} from "$env/static/private"
+
 export const actions = {
-    default: async ({request}) => {
+    default: async ({request, cookies}) => {
         // Get form data
         const form = await request.formData();
 
@@ -7,6 +14,31 @@ export const actions = {
         const username = form.get('username');
         const password = form.get('password');
 
+        try {
+            const insertion = await db.insert(users).values({
+                username: username,
+                password: await argon2.hash(password)
+            }).returning({ id: users.id });
+            const userId = insertion[0].id;
+            const secret = jose.base64url.decode(AUTH_SECRET)
+            const jwt = await new jose.EncryptJWT({ 'id': userId })
+                .setProtectedHeader({ alg: 'dir', enc: 'A128CBC-HS256' })
+                .setIssuedAt()
+                .setIssuer('yata')
+                .setExpirationTime('24h')
+                .encrypt(secret)
+            cookies.set("auth", jwt, {
+                path: '/',
+                sameSite: 'strict'
+            });
+        } catch (e) {
+            return fail(400, {
+                error: {
+                    message: "Unable to create an account with those credentials"
+                }
+            })
+        }
 
+        throw redirect(303, "/dashboard");
     }
 }
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,15 +2,35 @@
 import {Input} from "$lib/components/ui/input";
 import {Button} from "$lib/components/ui/button";
 import {enhance} from "$app/forms";
+import {Card, CardTitle, CardHeader, CardDescription, CardContent} from "$lib/components/ui/card";
+import {Alert, AlertDescription, AlertTitle} from "$lib/components/ui/alert";
+import {AlertCircle} from "lucide-svelte";
+
+export let form;
+
 </script>
 
-<div class="flex h-screen justify-center items-center">
-    <div class="border-white border p-8 rounded-3xl">
-        <h2 class="text-2xl mb-3 pl-3">Register</h2>
-        <form method="post" class="flex space-y-3 flex-col" use:enhance>
-            <Input class="md:w-72" name="username" type="test" placeholder="Username" required />
-            <Input class="md:w-72" name="password" type="password" placeholder="Password" required />
-            <Button type="submit">Register</Button>
-        </form>
-    </div>
+<div class="flex flex-col h-screen justify-center items-center space-y-3">
+    {#if form?.error}
+        <Alert class="max-w-80">
+            <AlertCircle class="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+                {form.error.message}
+            </AlertDescription>
+        </Alert>
+    {/if}
+    <Card>
+        <CardHeader>
+            <CardTitle>Register</CardTitle>
+            <CardDescription>Start tracking your time today!</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <form method="post" class="flex space-y-6 flex-col" use:enhance>
+                <Input class="md:w-72" name="username" type="test" placeholder="Username" required />
+                <Input class="md:w-72" name="password" type="password" placeholder="Password" required />
+                <Button type="submit">Register</Button>
+            </form>
+        </CardContent>
+    </Card>
 </div>
