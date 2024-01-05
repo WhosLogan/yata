649ae30b8b1c4b9ab387a985267903d15e803063
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-01-04 23:35:54 -0500

    Add comments

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -1,8 +1,11 @@
 import {redirect} from "@sveltejs/kit";
 
 export const load = ({cookies}) => {
+    // Delete the auth cookie
     cookies.delete('auth', {
         path: '/'
     })
+
+    // Redirect to the homepage
     throw redirect(303, '/')
 }
