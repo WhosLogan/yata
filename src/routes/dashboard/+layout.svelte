bd8b3bcade0ca641b61d081de6f27d1a946d900b
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-01-05 01:19:35 -0500

    Finish layout of navbar and the dashboard

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+import {Button} from "$lib/components/ui/button";
+</script>
+
+<div class="flex border p-3 pl-5 m-3 rounded-3xl items-center justify-between">
+    <div>
+        <a class="text-2xl" href="/dashboard">Yata</a>
+    </div>
+    <div>
+        <a data-sveltekit-preload-data="false" href="/logout">
+            <Button variant="outline">Logout</Button>
+        </a>
+    </div>
+</div>
+
+<slot />
